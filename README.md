5b921da977902f48dddc43f62b8a3f5f6f7f6b96
PythonNut <PythonNut@users.noreply.github.com>
2017-01-05 05:35:28 +0000

    Add basic README and commentary

---
+++ README.md
@@ -0,0 +1,6 @@
+# Historian.el
+
+Historian.el stores the results of completing-read and similar
+functions persistently. This provides a way to give completion
+candidates that are more frequently or more recently used a better
+position in the candidates list.
--- historian.el
+++ historian.el
@@ -25,7 +25,10 @@
 
 ;;; Commentary:
 
-;; TODO: Write commentary
+;; Historian.el stores the results of completing-read and similar
+;; functions persistently. This provides a way to give completion
+;; candidates that are more frequently or more recently used a better
+;; position in the candidates list.
 
 ;;; Code:
 
