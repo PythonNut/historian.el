bc2a808e7176d46682b5fc7f576f1388d84ffeae
PythonNut <PythonNut@users.noreply.github.com>
2017-01-12 02:05:58 +0000

    Add ivy dependency

--- historian-ivy.el
+++ historian-ivy.el
@@ -6,7 +6,7 @@
 ;; Keywords: convenience, ivy
 ;; Version: 20170111
 ;; URL: https://github.com/PythonNut/historian.el
-;; Package-Requires: ((emacs "24.4") (historian "20170111"))
+;; Package-Requires: ((emacs "24.4") (historian "20170111") (ivy "0.8.0"))
 
 ;;; License:
 
